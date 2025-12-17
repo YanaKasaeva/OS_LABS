c120e49e18e323e86a9b0bf46080e8bf6bd1b26b
yanakasaeva <yanakasaeva2006@gmail.com>
2025-12-17 22:32:22 +0300

    fix

--- CP/src/buddy.c
+++ CP/src/buddy.c
@@ -22,6 +22,7 @@ static size_t size_to_order(size_t size) {
     size_t order = MIN_ORDER;
     size_t block_size = 1UL << order;
     while (block_size < size + sizeof(BuddyBlock)) {
+        if (order >= MAX_ORDER) return 0;
         block_size <<= 1;
         order++;
     }
@@ -39,14 +40,27 @@ static BuddyBlock* get_buddy(void *base, BuddyBlock *block) {
 }
 
 Allocator* buddy_create(void *memory, size_t size) {
+    if (size < (1UL << MIN_ORDER) + sizeof(Allocator)) return NULL;
+    
     Allocator *a = (Allocator*)memory;
     memset(a->free_lists, 0, sizeof(a->free_lists));
-    a->memory_start = memory;
+    
+    // ИСПРАВЛЕНИЕ 1: memory_start указывает на начало пользовательской памяти
+    a->memory_start = (char*)memory + sizeof(Allocator);
+    
+    // ИСПРАВЛЕНИЕ 2: total_memory - только пользовательская память
     a->total_memory = size - sizeof(Allocator);
+    
+    // ИСПРАВЛЕНИЕ 3: used_memory начинается с 0
     a->used_memory = 0;
 
-    size_t order = size_to_order(a->total_memory);
-    BuddyBlock *b = (BuddyBlock*)((char*)memory + sizeof(Allocator));
+    size_t max_possible = a->total_memory;
+    size_t order = MIN_ORDER;
+    while (order < MAX_ORDER && (1UL << (order + 1)) <= max_possible) {
+        order++;
+    }
+    
+    BuddyBlock *b = (BuddyBlock*)a->memory_start;
     b->next = NULL;
     b->order = order;
     a->free_lists[order] = b;
@@ -56,6 +70,8 @@ Allocator* buddy_create(void *memory, size_t size) {
 
 void* buddy_alloc(Allocator *a, size_t size) {
     size_t order = size_to_order(size);
+    if (order == 0) return NULL;
+    
     size_t i = order;
 
     while (i <= MAX_ORDER && a->free_lists[i] == NULL) i++;
@@ -80,7 +96,7 @@ void* buddy_alloc(Allocator *a, size_t size) {
 }
 
 void buddy_free(Allocator *a, void *ptr) {
-    if (!ptr) return;
+    if (!ptr || !a) return;
 
     BuddyBlock *block = (BuddyBlock*)((char*)ptr - sizeof(BuddyBlock));
     size_t order = block->order;
@@ -88,19 +104,21 @@ void buddy_free(Allocator *a, void *ptr) {
     a->used_memory -= (1UL << order);
 
     while (order < MAX_ORDER) {
-        BuddyBlock **list = &a->free_lists[order];
-        BuddyBlock *prev = NULL;
-        BuddyBlock *cur = *list;
         BuddyBlock *buddy = get_buddy(a->memory_start, block);
+        BuddyBlock **list = &a->free_lists[order];
+        BuddyBlock *cur = *list;
+        BuddyBlock *prev = NULL;
         int merged = 0;
 
         while (cur) {
             if (cur == buddy) {
                 if (prev) prev->next = cur->next;
                 else *list = cur->next;
-
-                if ((uintptr_t)block > (uintptr_t)cur) block = cur;
-
+                
+                if ((uintptr_t)block > (uintptr_t)buddy) {
+                    block = buddy;
+                }
+                
                 order++;
                 block->order = order;
                 merged = 1;
@@ -121,5 +139,13 @@ size_t buddy_get_used_memory(Allocator *a) {
 }
 
 size_t buddy_get_free_memory(Allocator *a) {
-    return a->total_memory - a->used_memory;
+    size_t free_mem = 0;
+    for (int i = MIN_ORDER; i <= MAX_ORDER; i++) {
+        BuddyBlock *block = a->free_lists[i];
+        while (block) {
+            free_mem += (1UL << i);
+            block = block->next;
+        }
+    }
+    return free_mem;
 }
--- CP/src/main.c
+++ CP/src/main.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #define MEMORY_SIZE (1024*1024)
-#define N 100000
+#define N 10000
 
 typedef struct {
     double alloc_time;
     double free_time;
     size_t used_mem;
     size_t free_mem;
+    int successful_allocs;
 } Stats;
 
 static Stats test_allocator(const char *name,
@@ -20,26 +23,34 @@ static Stats test_allocator(const char *name,
                             void  (*free_func)(Allocator*, void*),
                             size_t (*get_used)(Allocator*),
                             size_t (*get_free)(Allocator*)) {
-    (void)name;
-
-    void *ptrs[N];
+    void *ptrs[N] = {0};
     size_t sizes[N];
-    for (int i = 0; i < N; i++) sizes[i] = (rand() % 256) + 16;
+    int successful_allocs = 0;
+    
+    for (int i = 0; i < N; i++) {
+        sizes[i] = (rand() % 112) + 16;
+    }
 
     Stats s = {0};
 
     clock_t start = clock();
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         ptrs[i] = alloc_func(A, sizes[i]);
+        if (ptrs[i]) successful_allocs++;
+    }
     clock_t end = clock();
     s.alloc_time = (double)(end - start) / CLOCKS_PER_SEC;
-
+    s.successful_allocs = successful_allocs;
+    
     s.used_mem = get_used(A);
     s.free_mem = get_free(A);
 
     start = clock();
-    for (int i = 0; i < N; i++)
-        free_func(A, ptrs[i]);
+    for (int i = 0; i < N; i++) {
+        if (ptrs[i]) {
+            free_func(A, ptrs[i]);
+        }
+    }
     end = clock();
     s.free_time = (double)(end - start) / CLOCKS_PER_SEC;
 
@@ -49,36 +60,47 @@ static Stats test_allocator(const char *name,
 int main(void) {
     srand((unsigned)time(NULL));
 
-    void *mem_ff = malloc(MEMORY_SIZE);
-    void *mem_bd = malloc(MEMORY_SIZE);
+    size_t page_size = sysconf(_SC_PAGESIZE);
+    size_t aligned_size = ((MEMORY_SIZE + page_size - 1) / page_size) * page_size;
+    
+    void *mem_ff = mmap(NULL, aligned_size, 
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void *mem_bd = mmap(NULL, aligned_size,
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    
+    if (mem_ff == MAP_FAILED || mem_bd == MAP_FAILED) {
+        perror("mmap failed");
+        return 1;
+    }
 
-    Allocator *ff = freelist_create(mem_ff, MEMORY_SIZE);
-    Allocator *bd = buddy_create(mem_bd, MEMORY_SIZE);
+    Allocator *ff = freelist_create(mem_ff, aligned_size);
+    Allocator *bd = buddy_create(mem_bd, aligned_size);
 
     Stats s_ff = test_allocator("Free List", ff, freelist_alloc, freelist_free,
                                 freelist_get_used_memory, freelist_get_free_memory);
     Stats s_bd = test_allocator("Buddy", bd, buddy_alloc, buddy_free,
                                 buddy_get_used_memory, buddy_get_free_memory);
 
-    printf("=== Сравнение аллокаторов ===\n");
-    printf("%-10s | %-11s | %-10s | %-10s\n",
+    printf("%-10s | %-11s | %-12s | %-12s\n",
            "Allocator", "Utilization", "Alloc Time", "Free Time");
-    printf("--------------------------------------------------\n");
+    printf("-----------------------------------------------------\n");
 
-    printf("%-10s | %-10.2f%% | %-10.6f | %-10.6f\n",
+    printf("%-10s | %-10.2f%% | %-12.6f | %-12.6f\n",
            "Free List",
-           100.0 * s_ff.used_mem / (s_ff.used_mem + s_ff.free_mem),
+           100.0 * s_ff.used_mem / aligned_size,
            s_ff.alloc_time,
            s_ff.free_time);
 
-    printf("%-10s | %-10.2f%% | %-10.6f | %-10.6f\n",
+    printf("%-10s | %-10.2f%% | %-12.6f | %-12.6f\n",
            "Buddy",
-           100.0 * s_bd.used_mem / (s_bd.used_mem + s_bd.free_mem),
+           100.0 * s_bd.used_mem / aligned_size,
            s_bd.alloc_time,
            s_bd.free_time);
 
-    free(mem_ff);
-    free(mem_bd);
+    munmap(mem_ff, aligned_size);
+    munmap(mem_bd, aligned_size);
 
     return 0;
 }
